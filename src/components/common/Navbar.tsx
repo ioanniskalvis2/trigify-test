97bb08ff8fa97ea846826e3e3ce2da7887c43cd6
ioanniskalvis <ioanniskalvis@hotmail.com>
2024-03-31 23:22:15 +0100

    Add Clerk authentication

---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,39 @@
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
+
+const Navbar = () => {
+  const { user } = useUser();
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="navbar-center">
+          <a
+            className="btn btn-ghost text-xl"
+            href="https://www.trigify.io/"
+            target="blank"
+          >
+            Trigify.io
+          </a>
+        </div>
+      </div>
+
+      <div className="navbar-end">
+        {!user ? (
+          <>
+            <SignUpButton>
+              <button className="btn btn-neutral">Sign Up</button>
+            </SignUpButton>
+            <div className="divider">or</div>
+            <SignInButton>
+              <button className="btn btn-neutral">Sign In</button>
+            </SignInButton>
+          </>
+        ) : (
+          <UserButton afterSignOutUrl="/" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
